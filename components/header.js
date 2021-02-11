7ba203f1515f29844cbfae669f04b0cc945ab3c9
Huong Dang <huongd97@knights.ucf.edu>
2021-02-10 23:39:55 -0500

    cors blocked it, work on making cors unblock it on server-side

--- components/header.js
+++ components/header.js
@@ -1,8 +1,21 @@
 import LoginButton from "./login";
 import LogoutButton from "./logout";
-import { useAuth0 } from "@auth0/auth0-react";
-export default function Header() {
-    const { isAuthenticated } = useAuth0();
-    const button = isAuthenticated ? <LogoutButton /> : <LoginButton />;
-    return button;
+import { withAuth0 } from "@auth0/auth0-react";
+import { Component } from "react";
+import axios from "axios";
+
+class Header extends Component {
+    async componentDidMount() {
+        const result = await axios.get("https://dear-genie-api.vercel.app/");
+        console.log(result);
+    }
+
+    render() {
+        const { isAuthenticated } = this.props.auth0;
+        const button = isAuthenticated ? <LogoutButton /> : <LoginButton />;
+
+        return button;
+    }
 }
+
+export default withAuth0(Header);
