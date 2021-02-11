7d6ca1328df7cdc91ca68d505da5e5b5b2c29a9d
Huong Dang <huongd97@knights.ucf.edu>
2021-02-10 23:06:32 -0500

    remove

--- components/profile.js
+++ components/profile.js
@@ -1,42 +1,7 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { useEffect, useState } from "react";
 
 export default function Profile() {
-    const {
-        user,
-        isAuthenticated,
-        isLoading,
-        getAccessTokenSilently,
-    } = useAuth0();
-    const [userMetadata, setUserMetadata] = useState(null);
-    useEffect(() => {
-        const getUserMetadata = async () => {
-            const domain = process.env.NEXT_PUBLIC_DOMAIN;
-
-            try {
-                const accessToken = await getAccessTokenSilently({
-                    scope: "read:current_user",
-                });
-                console.log(`accessToken ${accessToken}`);
-
-                const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user.sub}`;
-
-                const metadataResponse = await fetch(userDetailsByIdUrl, {
-                    headers: {
-                        Authorization: `Bearer ${accessToken}`,
-                    },
-                });
-
-                const { user_metadata } = await metadataResponse.json();
-
-                setUserMetadata(user_metadata);
-            } catch (e) {
-                console.log(e.message);
-            }
-        };
-
-        getUserMetadata();
-    }, []);
+    const { user, isAuthenticated, isLoading } = useAuth0();
 
     if (isLoading) {
         return <div>Loading ...</div>;
@@ -47,11 +12,6 @@ export default function Profile() {
                     <img src={user.picture} alt={user.name} />
                     <h2>{user.name}</h2>
                     <p>{user.email}</p>
-                    {userMetadata ? (
-                        <pre>{JSON.stringify(userMetadata, null, 2)}</pre>
-                    ) : (
-                        "No user metadata defined"
-                    )}
                 </div>
             )
         );
